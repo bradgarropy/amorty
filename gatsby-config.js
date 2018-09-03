6442b7b77f36371f7daba3d12825f12157c7d245
Brad Garropy <bradgarropy@gmail.com>
2018-09-03 11:06:17 -0500

    basic changes.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
+    siteMetadata: {
+        title: "ammorty",
+    },
+    plugins: [
+        {
+            resolve: "gatsby-plugin-react-helmet",
+        },
+    ],
 }
