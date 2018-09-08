95a0335f1e7f6ab429af7489ff80204857055c9c
Brad Garropy <bradgarropy@gmail.com>
2018-09-08 13:10:10 -0500

    form layout.

--- src/components/AmortizationForm.jsx
+++ src/components/AmortizationForm.jsx
@@ -12,56 +12,41 @@ const AmortizationForm = ({amount, rate, term, date, onChange}) => {
 
         <form className="amortization-form">
 
-            <div>
 
-                <label>Principal</label>
-                <input
-                    type="number"
-                    name="amount"
-                    min={0}
-                    value={amount}
-                    onChange={onChange}
-                />
+            <label>Principal</label>
+            <input
+                type="number"
+                name="amount"
+                min={0}
+                value={amount}
+                onChange={onChange}
+            />
 
-            </div>
+            <label>Rate</label>
+            <input
+                type="number"
+                name="rate"
+                min={0}
+                value={rate}
+                onChange={onChange}
+            />
 
-            <div>
+            <label>Term</label>
+            <input
+                type="number"
+                name="term"
+                min={0}
+                value={term}
+                onChange={onChange}
+            />
 
-                <label>Rate</label>
-                <input
-                    type="number"
-                    name="rate"
-                    min={0}
-                    value={rate}
-                    onChange={onChange}
-                />
-
-            </div>
-
-            <div>
-
-                <label>Term</label>
-                <input
-                    type="number"
-                    name="term"
-                    min={0}
-                    value={term}
-                    onChange={onChange}
-                />
-
-            </div>
-
-            <div>
-
-                <label>Date</label>
-                <input
-                    name="date"
-                    type="date"
-                    value={format(date, "YYYY-MM-DD")}
-                    onChange={onChange}
-                />
-
-            </div>
+            <label>Date</label>
+            <input
+                name="date"
+                type="date"
+                value={format(date, "YYYY-MM-DD")}
+                onChange={onChange}
+            />
 
         </form>
 
